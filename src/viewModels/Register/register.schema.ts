856d7def93d6a8a3787177c5811c8e2667c67d0e
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-12-26 00:15:30 -0300

    Register form schema

---
+++ src/viewModels/Register/register.schema.ts
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+
+export const registerSchema = yup.object({
+  name: yup
+    .string()
+    .required("Nome é obrigatório")
+    .min(4, "Nome deve ter pelo menos 4 caracteres"),
+  email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
+  phone: yup
+    .string()
+    .required("Telefone é obrigatório")
+    .matches(/^\d{11}$/, "Telefone deve ter 11 dígitos (DDD + número)"),
+  password: yup
+    .string()
+    .required("Senha é obrigatória")
+    .min(6, "Senha deve ter pelo menos 6 caracteres"),
+  passwordConfirm: yup
+    .string()
+    .required("Confirmação de senha é obrigatória")
+    .oneOf([yup.ref("password"), "Senhas não coincidem"]),
+});
