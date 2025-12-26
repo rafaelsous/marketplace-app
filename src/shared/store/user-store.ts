a61c39e47c3dff4b7297e5c0af6bdca333e9fe2e
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-12-26 18:00:12 -0300

    Persisting user data

--- src/shared/store/user-store.ts
+++ src/shared/store/user-store.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { UserI } from "../interfaces/user";
 
@@ -22,11 +24,24 @@ export interface UserStore {
   updateTokens: (updateTokensData: UpdateTokensParams) => void;
 }
 
-export const useUserStore = create<UserStore>()((set) => ({
-  user: null,
-  token: null,
-  refreshToken: null,
-  logout: () => {},
-  setSession: (sessionData) => set({ ...sessionData }),
-  updateTokens: (updateTokensData) => set({ ...updateTokensData }),
-}));
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      refreshToken: null,
+      logout: () =>
+        set({
+          user: null,
+          token: null,
+          refreshToken: null,
+        }),
+      setSession: (sessionData) => set({ ...sessionData }),
+      updateTokens: (updateTokensData) => set({ ...updateTokensData }),
+    }),
+    {
+      name: "marketplace-auth",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
