dbc8a5977e3821a2569e9236cede8203a14fb60f
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-01-04 15:31:37 -0300

    Basic styling of the product card

--- src/viewModels/Home/components/ProductCard/ProductCard.view.tsx
+++ src/viewModels/Home/components/ProductCard/ProductCard.view.tsx
@@ -1,12 +1,39 @@
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { StarBold } from "@solar-icons/react-native";
+
+import { colors } from "@/styles/colors";
+
 import { useProductCardViewModel } from "./useProductCard.viewModel";
 
 export function ProductCardView({
   product,
 }: Readonly<ReturnType<typeof useProductCardViewModel>>) {
   return (
-    <View>
-      <Text>{product.name}</Text>
-    </View>
+    <TouchableOpacity className="w-[48%] h-[157px] mt-6 mb-2 bg-white rounded-xl overflow-hidden shadow-sm">
+      <View>
+        <Image
+          source={{ uri: product.photo }}
+          resizeMode="cover"
+          className="w-full h-[96px] rounded-md"
+        />
+
+        <View className="absolute top-0 right-0 px-2 py-1 flex-row items-center gap-1 rounded-b-lg rounded-r-none bg-white">
+          <StarBold size={12} color={colors["blue-base"]} />
+          <Text className="text-sm font-semibold text-gray-500">
+            {product.averageRating}
+          </Text>
+        </View>
+      </View>
+
+      <View className="p-3">
+        <Text className="text-xs font-semibold" numberOfLines={2}>
+          {product.name}
+        </Text>
+
+        <View className="flex-row items-center justify-center">
+          <Text>R$ {product.value}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 }
