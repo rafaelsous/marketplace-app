fc135eb150b2d5e1f182b00c17abd6cf43cbf280
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-01-05 09:31:48 -0300

    Building an infinite query for product searches

---
+++ src/shared/queries/product/use-product-infinite.query.ts
@@ -0,0 +1,50 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { getProducts } from "@/shared/services/product.service";
+
+export function useProductInfinitQuery() {
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useInfiniteQuery({
+    queryFn: async ({ pageParam = 1 }) => {
+      try {
+        const response = await getProducts({
+          pagination: {
+            page: pageParam,
+            perPage: 10,
+          },
+        });
+
+        return response;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+    getNextPageParam: (lastPage) => {
+      return lastPage.page < lastPage.totalPages
+        ? lastPage.page + 1
+        : undefined;
+    },
+    initialPageParam: 1,
+    queryKey: ["products"],
+  });
+
+  return {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    refetch,
+    isRefetching,
+  };
+}
