91e5f2b80b596624bf4ff4e2d013f98fab3f9525
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-01-25 16:38:12 -0300

    Installing e setting up expo-notifications

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 
 export default {
   expo: {
+    android: {
+      package: "com.rafaelsousa.marketplaceapp",
+    },
     extra: {
       isPhysicalDevice: process.env.IS_PHYSICAL_DEVICE === "true",
     },
